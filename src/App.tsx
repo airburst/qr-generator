efc36927bcd1262f55f37e94c7206f61dadc35c3
Mark Fairhurst <mark.fairhurst@simplybusiness.co.uk>
2023-06-07 12:55:49 +0100

    feat: Download file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import Input from "@mui/base/Input";
+import { Text } from "@twilio-paste/text";
+import { Flex } from "@twilio-paste/flex";
+import { Box } from "@twilio-paste/box";
+import { Button } from "@twilio-paste/button";
+import { Label } from "@twilio-paste/label";
+import { Input } from "@twilio-paste/input";
+import { Switch } from "@twilio-paste/switch";
 import { qr } from "headless-qr";
 import { generateSvg } from "./utils/generateSvg";
 import { GridType } from "./types";
@@ -7,27 +13,68 @@ import "./App.css";
 
 function App() {
   // const [text, setText] = useState<string | null>(null);
-  const [text, setText] = useState<string | null>(
+  const [text, setText] = useState<string | undefined>(
     "https://quoter-airburst.vercel.app"
   );
+  const [spots, setSpots] = useState<boolean>(false);
+
+  const toggleSpots = () => setSpots(!spots);
+
+  const downloadSvgFile = () => {
+    const file = new Blob([svg], { type: "text/plain" });
+    const element = document.createElement("a");
+
+    element.href = URL.createObjectURL(file);
+    element.download = "qr-code-" + Date.now() + ".svg";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  };
+
   // Generate QR map of booleans
   const data = qr(text || "") as GridType;
   // Convert to SVG
-  const svg = generateSvg({ data, spots: true });
+  const svg = generateSvg({ data, spots });
 
   return (
     <>
-      <h1>QR Code Generator</h1>
+      <Text
+        as="h1"
+        fontWeight="fontWeightBold"
+        fontSize="fontSize80"
+        marginBottom="space80"
+      >
+        QR Code Generator
+      </Text>
 
-      <Input
-        aria-label="Enter url or text"
-        id="qr-value"
-        className="qr-value"
-        value={text}
-        onChange={(e) => setText(e.currentTarget.value)}
-      />
+      <Box textAlign="left">
+        <Label htmlFor="qr_code_text">Url or text to encode</Label>
+      </Box>
+      <Flex marginBottom="space80" vAlignContent="center">
+        <Flex grow vertical>
+          <Input
+            id="qr_code_text"
+            name="qr_code_text"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.currentTarget.value)}
+          />
+        </Flex>
+        <Flex>
+          <Box marginLeft="space40">
+            <Switch name="qrCodeStyle" checked={spots} onChange={toggleSpots}>
+              Spotty
+            </Switch>
+          </Box>
+        </Flex>
+      </Flex>
 
-      {text && <div dangerouslySetInnerHTML={{ __html: svg }} />}
+      <Box marginBottom="space80">
+        {text && <div dangerouslySetInnerHTML={{ __html: svg }} />}
+      </Box>
+
+      <Button id="download" variant="primary" onClick={downloadSvgFile}>
+        Download
+      </Button>
     </>
   );
 }
